c75c14ed491a8fbd7606d5bae98cf0ed93ba0586
hasansas <netter23@gmail.com>
2023-09-25 16:41:35 +0700

    ad qontact create broadcast service

--- api/qontak/router.js
+++ api/qontak/router.js
@@ -8,12 +8,23 @@ import QontakController from './controller'
 
 module.exports = function (app, router) {
   /**
-   * Create contact list
+   * Create broadcast recipient
    */
-  router.post('/v1/qontak/contacts',
+  router.post('/v1/qontak/broadcast/recipient',
     EXPRESS_VALIDATOR.body('name').not().isEmpty(),
     EXPRESS_VALIDATOR.body('contacts').not().isEmpty(),
     (req, res) => {
       QontakController({ req, res }).createContacts()
+    })
+
+  /**
+     * Create broadcast
+     */
+  router.post('/v1/qontak/broadcast',
+    EXPRESS_VALIDATOR.body('name').not().isEmpty(),
+    EXPRESS_VALIDATOR.body('message_template_id').not().isEmpty(),
+    EXPRESS_VALIDATOR.body('contact_list_id').not().isEmpty(),
+    (req, res) => {
+      QontakController({ req, res }).createBroadcast()
     })
 }
